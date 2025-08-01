2fdc5f8e7bf907e6b025879bc9398e869929d106
Pylyp Radionov <me@mr13.tech>
2025-08-01 12:49:07 +0700

    fix: build prod and test.

--- demo/www/healthcheck.js
+++
@@ -1,8 +0,0 @@
-// /www/healthcheck.js
-// This script checks if the Next.js server is running.
-// It's used by the HEALTHCHECK instruction in the Containerfile.
-const response = await fetch('http://localhost:3000')
-if (response.status !== 200) {
-  process.exit(1)
-}
-process.exit(0)
